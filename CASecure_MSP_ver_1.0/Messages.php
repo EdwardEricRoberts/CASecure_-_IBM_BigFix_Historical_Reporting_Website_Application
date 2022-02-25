ddab7a42295f846facc6f55bf8d33d79369eed17
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:40:40 -0500

    Create Messages.php

---
+++ CASecure_MSP_ver_1.0/Messages.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title>CASecure >Messages</title>
+
+<?php require 'includes/header.php'; ?>
+
+<?php require 'includes/alert.php'; ?>
+
+<?php require 'includes/navigation.php'; ?>
+
+<script>document.getElementById("infoNav").className = "current-menu-item";</script>
+
+<span class="breadcrumbs"> Information > Messages</span>
+<br>
+<div class="pagetitle">Messages</div>
+<div>
+	<table class="dashboxlong">
+	<tr>
+		<td><img src="SampleImages/Messages/yes_lock.jpg">
+		</td>
+		<td class="alertMessage">Vulnerability Alert: Microsoft release patch for Intel meltdown
+		</td>
+		<td>9:00am 1/3/2018
+		</td>
+		<td><img src="SampleImages/Messages/close.jpg">
+		</td>
+	</tr>
+	<tr>
+		<td><img src="SampleImages/Messages/yes_lock.jpg">
+		</td>
+		<td class="alertMessage">CAS Admin: New custom report is now available under your "Reports" tag
+		</td>
+		<td>12:34am 12/23/2017
+		</td>
+		<td><img src="SampleImages/Messages/close.jpg">
+		</td>
+	</tr>
+	<tr>
+		<td><img src="SampleImages/Messages/yes_lock.jpg">
+		</td>
+		<td class="alertMessage">CAS Admin: Servers decommissioned
+		</td>
+		<td>10:14am 11/3/2017
+		</td>
+		<td><img src="SampleImages/Messages/close.jpg">
+		</td>
+	</tr>
+
+
+	</tr>
+</table></div>
+
+<?php require 'includes/footer.php'; ?>
