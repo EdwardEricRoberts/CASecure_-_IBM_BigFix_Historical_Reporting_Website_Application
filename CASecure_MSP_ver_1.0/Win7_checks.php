b5b3c02af888cf6cfe1e1558fcb1a4f48bc318c9
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:57:31 -0500

    Create Win7_checks.php

---
+++ CASecure_MSP_ver_1.0/Win7_checks.php
@@ -0,0 +1,49 @@
+<?php
+require_once './includes/authenticate.php';
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      <title>CASecure > More > Compliance > PCI > DSS Windows 7 > Checks</title>
+
+	  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+      <script src="bootstrap.js"></script>
+      <link rel="stylesheet" href="bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="TABLE_FORMAT_1.css">
+	  <?php require 'includes/headerRevised_2.php'; ?>
+      <?php require 'includes/navigationRevised.php'; ?>
+<!-- start breadcrumbs -->                
+	  <div class="span-24">
+         <div style="background-color:#cacaca; width:100%; margin-top:2px; height:23px; line-height:23px;"> 
+            <span style="text-align:center;">
+               <img src="includes/breadcrumbs_start.jpg" style="height:23px;">
+               <span style="z-index:0; position:absolute;">More &nbsp;&raquo;&nbsp; <a href="ComplianceOverview.php">Compliance Overview</a> &nbsp;&raquo;&nbsp; <a href="PCI.php">PCI</a> &nbsp;&raquo;&nbsp;  <a href="PCI-DSSWin7.php">DSS Windows 7</a> &nbsp;&raquo;&nbsp;  Checks</span>
+            </span>
+         </div>
+      </div>
+<!-- end breadcrumbs -->
+
+      <br>
+      <div class="span-24 last">
+	     <div class="pagetitle2" style="margin-bottom:20px; margin-left:10px; float:left">DSS Windows 7 - Checks<br>
+		 </div>	  
+	  </div>
+
+
+<script>document.getElementById("complianceNav").className = "current-menu-item";</script>
+
+
+<div class="container">
+<div class="span-24 last">
+	<img src="SampleImages/Win7_checks/Win7_checks.jpg">
+
+
+</div>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<?php require 'includes/footer.php'; ?>
+
+</div>
+
