8acd628defcce9b64c6f3a96680b4dc6e19cabb1
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:36:58 -0500

    Create headerRevised_2.php

---
+++ CASecure_MSP_ver_1.0/includes/headerRevised_2.php
@@ -0,0 +1,91 @@
+
+
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      
+     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+    <!-- CSS for jquery datatables -->
+	   <link rel="stylesheet" type="text/css" href="DataTables/jquery.dataTables.min.css ">
+	   
+	<!-- core jquery library -->
+       <script src="jquery-3.3.1.min.js"></script>
+    <!-- javascript for the datatables ui -->
+	   <script type="text/javascript" src="DataTables/dataTables.jqueryui.min.js"></script>
+	<!-- javascript for datatables copy, csv, excel, pdf, print -->   
+       <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+       <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
+       <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+       <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+       <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
+       <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+	   
+	<!-- javascript for DataTables date sorting feature -->
+	   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js"></script>
+	   <script src="https://cdn.datatables.net/plug-ins/1.10.19/sorting/datetime-moment.js"></script>
+	   
+	<!-- javascript for percentage sorting in datatables -->
+       <script src="includes/percent.js"></script>
+	
+	
+	
+	
+    <!-- javascript required for data import and formatting of charts -->
+	   
+	   <script src="http://d3js.org/d3.v5.min.js"></script>
+	   <script src="d3LineChart.js"></script>
+       <script src="d3BarChart.js"></script>
+	   <script src="d3PieChart.js"></script>
+	   
+	<!-- css for formatting of data charts -->
+	   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <!-- javascript for dropdown selector -->
+       <script src="bootstrap.js"></script>
+	<!-- css for dropdown selector -->
+       <link rel="stylesheet" href="bootstrap.css">
+	<!-- javascript for the datatables functionality -->
+	     
+    <!-- CSS for blueprint page layout -->
+       <link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen" />
+       <link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print" />
+       <link rel="stylesheet" id="uncomp-core-ie" href="css/blueprint/ie.css" type="text/css" media="screen" />
+    <!-- CSS for local and custom CSS -->	  
+       <link rel="stylesheet" id="uncomp-core-ie" href="MSSP_CSS.css" type="text/css" />	  
+       <!--<link rel="stylesheet" id="plugin-debugger" href="debug.css" type="text/css" media="screen" />-->
+
+
+<script>
+	     var password = "AllieCat6";
+	     var server = "bigfix.internal.cassevern.com";
+</script>
+
+	
+   </head>
+
+   <body>
+      <div class="container">
+	    
+<!-- top banner graphic -->	
+         <div class="span-24"> 		
+	        <div style="position:relative;">
+               <img src="includes/banner.jpg">			
+               <div style="background-color:#185725; position:absolute; right:0; top:0; text-align:right; color:#fff; font-weight:bold; display:inline;">
+                  &nbsp;&nbsp;&nbsp;
+				  Welcome <?= htmlentities($_SESSION['firstname']); ?>
+				 <!-- Welcome Chris-->
+				  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;
+				  <?php if ($_SESSION['admin'] == 1) { ?>
+				  <a href="Administration.php" class="toprightmenu">Admin</a>
+				  &nbsp;&nbsp;|&nbsp;&nbsp;
+				  <?php } ?>
+				  <a href="Alerts.php" class="toprightmenu">Alerts</a>
+				  &nbsp;&nbsp;|&nbsp;&nbsp;
+				  <?php include_once './includes/logout_button.php'; "style=display:inline;"?>
+				  
+				  <!--<a href="#" class="toprightmenu">SignOut</a>-->
+				  &nbsp;&nbsp;&nbsp;
+               </div>
+            </div>
+         </div>   
+<!-- end top banner -->		
+   
