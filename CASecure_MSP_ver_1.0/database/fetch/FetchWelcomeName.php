d31dad70ae406323eaf04c7e503e0b328edd05bb
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:30:26 -0500

    Create FetchWelcomeName.php

---
+++ CASecure_MSP_ver_1.0/database/fetch/FetchWelcomeName.php
@@ -0,0 +1,115 @@
+<?php
+	
+	header('Content-type: application/xml');
+	
+	$currentUser = $_GET['cid'];
+	
+	$db_host = "localhost";
+	$db_name = "CASecure1";
+	$db_username = "postgres";
+	$db_password = "abc.123";
+	
+	$xml= new SimpleXMLElement('<PDO/>'); 
+	$connectionXML = $xml->addChild('Connection');
+	try {
+		$connectResultXML = $connectionXML->addChild('Result');
+		
+		$db = new PDO('pgsql:host='.$db_host.';dbname='.$db_name,$db_username,$db_password);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$connectResultXML->addChild('Status', 'Connected');
+		$connectResultXML->addChild('Message', "Database Connection Successful");
+		$connectResultXML->addChild('Host', $db_host);
+		$connectResultXML->addChild('Database', $db_name);
+		
+		try {
+			$queryXML = $xml->addChild('Query');
+			
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+			$db->beginTransaction();
+				
+				$metaXML = $queryXML->addChild('Meta');
+				$metaXML->addChild('Name', "Fetch Welcome Name");
+				$metaXML->addChild('Description', "This query returns the given first name of the current user.");
+				$paramsXML = $metaXML->addChild('Parameters');
+				$param1XML = $paramsXML->addChild('Parameter');
+				$param1XML->addChild('Name', 'Current User ID');
+				$param1XML->addChild('URL', 'cid');
+				$param1XML->addChild('Value', $currentUser);
+				$param1XML->addChild('Description', 'The User ID of the current User.');
+				$resultXML = $queryXML->addChild('Result');
+				
+				$fetchWelcomeNameSQL = 
+					"SELECT welcome_name ".
+					"FROM users ".
+					"WHERE user_id = :currentUser;";
+				$fetchWelcomeNameQuery = $db->prepare($fetchWelcomeNameSQL);
+				$fetchWelcomeNameQuery->bindParam(":currentUser", $currentUser, PDO::PARAM_STR);
+				$start = microtime(true);
+				$fetchWelcomeNameQuery->execute();
+				$end = microtime(true);
+				$welcomeName = ($fetchWelcomeNameQuery->fetch(PDO::FETCH_ASSOC))['welcome_name'];
+				
+				$tuple = $resultXML->addChild('user');
+				$tuple->addChild('welcome_name', $welcomeName);
+				
+				$evaluationXML = $queryXML->addChild('Evaluation');
+				$evaluationXML->addChild('Time', round((($end - $start) * 1000), 3).'ms');
+				$evaluationXML->addChild('Number_of_Results', 1);
+				
+			$db->commit();
+		}
+		catch (\PDOException $e) {
+			$errorXML = $queryXML->addChild('Error');
+			$errorXML->addChild('Code', $fetchWelcomeNameQuery->errorCode());
+			$errorXML->addChild('Code', $e->getCode());//$fetchAlertsByUserGroupQuery->errorCode());
+			$errorXML->addChild('Details', $e->getMessage());
+			$db->rollback();
+		}
+	}
+	catch(PDOException $e) {
+		$connectResultXML->addChild('Status', 'Failure');
+		$connectResultXML->addChild('Message', "Failed to Connect to Database.  Please Email Site Administrator.");
+		$connectResultXML->addChild('Host', $db_host);
+		$connectResultXML->addChild('Database', $db_name);
+		
+		$connectErrorXML = $connectionXML->addChild('Error');
+		$connectErrorXML->addChild('Code', $e->getCode());
+		$connectErrorXML->addChild('Details', $e->getMessage());
+	}
+	catch(Exception $e) {
+		$connectResultXML->addChild('Status', 'Failure');
+		$connectResultXML->addChild('Message', "An Unexpected Error Occured.  Please try again. If Issue Persists, Please Email Site Administrator.");
+		$connectResultXML->addChild('Host', $db_host);
+		$connectResultXML->addChild('Database', $db_name);
+		
+		$connectErrorXML = $connectionXML->addChild('Error');
+		$connectErrorXML->addChild('Error_Code', $e->getCode());
+		$connectErrorXML->addChild('Details', $e->getMessage());
+	}
+	
+	echo $xml->asXML();
+
+	
+	function errorHandle(Exception $e) {
+		$trace = $e->getTrace();
+		if ($trace[0]['class'] != "") {
+			$class = $trace[0]['class'];
+		}
+			$method = $trace[0]['function'];
+			$file = $trace[0]['file'];
+			$line = $trace[0]['line'];
+			$Exception_Output = $e->getMessage().
+			"<br />Class & Method: ".$class."->".$method.
+			"<br />File: ".$file.
+			"<br />Line: ".$line;
+			return $Exception_Output;
+	}
+	
+	function ArrayBinder(&$pdoStatement, &$array) {  //& operator is used because function binds values
+		foreach ($array as $k=>$v) { // short for "key value"
+			$pdoStatement->bindValue(':'.$k, $v);
+		}
+	}
+?>
