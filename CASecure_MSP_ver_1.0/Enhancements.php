9958230c1b5642c6d4cee5a483ad75786ee8d818
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:34:37 -0500

    Create Enhancements.php

---
+++ CASecure_MSP_ver_1.0/Enhancements.php
@@ -0,0 +1,24 @@
+<?php
+require_once './includes/authenticate.php';
+?>
+
+<html>
+<head>
+<title>CASecure >Enhancements</title>
+
+<?php require 'includes/headerRevised_2.php'; ?>
+
+<!-- <?php //require 'includes/alert.php'; ?> -->
+
+<?php require 'includes/navigationRevised.php'; ?>
+
+<script>document.getElementById("infoNav").className = "current-menu-item";</script>
+
+<span class="breadcrumbs"> Information > Enhancements</span>
+<br>
+<div class="pagetitle">Enhancements</div>
+<div class="dashbox">
+Enhancements coming soon
+</div>
+
+<?php require 'includes/footer.php'; ?>
