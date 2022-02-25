9ad922a2e9c9b8ab34afea766a245b0f4dbc0b4e
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:38:31 -0500

    Create navigationRevised.php

---
+++ CASecure_MSP_ver_1.0/includes/navigationRevised.php
@@ -0,0 +1,47 @@
+<!-- start navigation -->
+         <div class="span-24" style="background-color:#185725;">    
+            <div class="span-24" style="margin-left:-18px; z-index:4;">
+               <nav id="primary_nav_wrap">
+                  <ul>
+                     <li id="dashboardNav"><a href="Dashboards.php">Dashboard</a></li>
+                   <!--  <li id="patchingNav" style="z-index:4;"><a href="PatchingOverview.php">Patching</a> -->
+                        <ul>
+                           
+                           <li><a href="Devices.php">Devices</a></li>
+                        </ul>
+	                 </li>
+                     
+					 
+                     
+                     <li id="reportingNav"><a href="Reporting.php">Reporting</a></li>
+                     <li id="infoNav" style="z-index:4;"><a href="#">Information</a>
+                        <ul>
+                           <li style="z-index:4;"><a href="Requests.php">Requests</a></li>
+                           <li style="z-index:4;"><a href="Alerts.php">Alerts</a></li>
+                        </ul>
+                     </li>
+					<!-- <li id="moreNav"><a href="#">More</a>
+					    <ul>
+						   <li style="z-index:4;"><a href="ComplianceOverview.php">Compliance Overview</a>
+						      <ul>
+							     
+								 <li style="z-index:4;"><a href="PCI.php">PCI</a>
+								    <ul>
+									   <li style="z-index:4;"><a href="PCI-DSSWin2008.php">PCI-DSS Windows 2008</a></li>
+									   <li style="z-index:4;"><a href="PCI-DSSWin7.php">PCI-DSS Windows 7</a></li>
+									</ul>
+								 </li>
+								 <li style="z-index:4;"><a href="#">CIS</a></li>
+								 <li style="z-index:4;"><a href="#">USGCB</a></li>
+							  </ul>
+						   </li>
+						   <li id="inventoryNav"><a href="#">Inventory</a></li>
+						</ul>
+					 </li>
+					 -->
+                  </ul>
+               </nav>	
+            </div>
+         </div>	
+                      
+<!-- end navigation -->
