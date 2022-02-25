0d1fbeca9199e26effaa328788f7fcb9352b0696
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:36:46 -0500

    Create header2.php

---
+++ CASecure_MSP_ver_1.0/includes/header2.php
@@ -0,0 +1,63 @@
+<!--
+<html>
+<head>
+<title>CASecure >Dashboards</title>
+-->
+
+
+<!-- steve includes -->
+
+<script src="jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/DataTables/datatables.css">
+<script type="text/javascript" charset="utf8" src="DataTables/datatables.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.jqueryui.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.15/js/dataTables.jqueryui.min.js"></script>
+
+<!-- end steve includes -->
+
+
+
+
+<!--<script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>-->
+<!--<script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>-->
+<link rel="stylesheet" type="text/css" href="MSSP_CSS.css">
+<!--<link rel="stylesheet" type="text/css" href="MSSP_CSS_bp.css">-->
+<link rel="stylesheet" type="text/css" href="css/blueprint/screen.css" media="screen, projection">
+<link rel="stylesheet" type="text/css" href="css/blueprint/print.css" media="print">
+<link rel="stylesheet" type="text/css" href="css/blueprint/ie.css" media="screen, projection">
+
+<!-- <link href="includes/jquery.modal/jquery.modal.css" type="text/css" rel="stylesheet" /> -->
+<!--<script src="http://code.jquery.com/jquery-latest.min.js"></script>-->
+<!-- <script src="includes/jquery.modal/jquery.modal.min.js"></script>  -->
+
+<!-- Remember to include jQuery :) -->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>-->
+
+<!-- jQuery Modal -->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+
+<!--
+<script src="jquery-3.3.1.min.js"></script>
+-->
+<!--
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+-->
+<!--
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="js/jquery.ajax-cross-origin.min.js"></script>
+-->
+</head>
+<body onload="loadReports()">
+<div class="container">
+
+	<div class="banner2">
+		<div class="span-6"><img src="includes/casecure_logo.jpg" class="logo2"></div>
+		<div class="prepend-20 last">
+			<span class="webportal2"><br>Web Portal</span><br>
+			<span class="welcome2">The Tech Giant<br>Welcome Chris</span>
+		</div>
+	</div>
+	
