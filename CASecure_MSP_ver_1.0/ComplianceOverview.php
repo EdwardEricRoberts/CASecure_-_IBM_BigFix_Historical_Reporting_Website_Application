e90c75739e27220ed13319d70cc8ac05cdfacd06
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:26:15 -0500

    Create ComplianceOverview.php

---
+++ CASecure_MSP_ver_1.0/ComplianceOverview.php
@@ -0,0 +1,55 @@
+<?php
+require_once './includes/authenticate.php';
+?>
+
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      <title>CASecure > Compliance Overview</title>
+
+	  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+      <script src="bootstrap.js"></script>
+      <link rel="stylesheet" href="bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="TABLE_FORMAT_1.css">
+	  <?php require 'includes/headerRevised_2.php'; ?>
+      <?php require 'includes/navigationRevised.php'; ?>
+<!-- start breadcrumbs -->                
+	  <div class="span-24">
+         <div style="background-color:#cacaca; width:100%; margin-top:2px; height:23px; line-height:23px;"> 
+            <span style="text-align:center;">
+               <img src="includes/breadcrumbs_start.jpg" style="height:23px;">
+               <span style="z-index:0; position:absolute;">More &nbsp;&raquo;&nbsp; Compliance Overview</span>
+            </span>
+         </div>
+      </div>
+<!-- end breadcrumbs -->
+
+      <br>
+      <div class="span-24 last">
+	     <div class="pagetitle2" style="margin-bottom:20px; margin-left:10px; float:left">Compliance Overview <br>
+		 </div>	  
+	  </div>
+
+
+<script>document.getElementById("complianceNav").className = "current-menu-item";</script>
+
+
+<div class="container">
+<div class="span-11">
+
+	<img src="SampleImages/ComplianceOverview/CO_1.jpg">
+</div>
+<div class="span-11 last">
+	<img src="SampleImages/ComplianceOverview/CO_2.jpg">
+</div>
+<div class="span-21 prepend-5 last">
+	<img src="SampleImages/ComplianceOverview/CO_3.jpg">
+</div>
+<br><br><br><br><br><br>
+
+<?php require 'includes/footer.php'; ?>
+</div>
+<script>document.getElementById("moreNav").className = "current-menu-item";</script>
+
