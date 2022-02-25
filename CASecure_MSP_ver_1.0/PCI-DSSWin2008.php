465e89e03bf934a3a9e90cbfb36b40859485e99e
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:45:19 -0500

    Create PCI-DSSWin2008.php

---
+++ CASecure_MSP_ver_1.0/PCI-DSSWin2008.php
@@ -0,0 +1,61 @@
+<?php
+require_once './includes/authenticate.php';
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+   <head>
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+      <title>CASecure > More > Compliance > PCI > DSS Windows 2008</title>
+
+	  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+      <script src="bootstrap.js"></script>
+      <link rel="stylesheet" href="bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="TABLE_FORMAT_1.css">
+	  <?php require 'includes/headerRevised_2.php'; ?>
+      <?php require 'includes/navigationRevised.php'; ?>
+<!-- start breadcrumbs -->                
+	  <div class="span-24">
+         <div style="background-color:#cacaca; width:100%; margin-top:2px; height:23px; line-height:23px;"> 
+            <span style="text-align:center;">
+               <img src="includes/breadcrumbs_start.jpg" style="height:23px;">
+               <span style="z-index:0; position:absolute;">More &nbsp;&raquo;&nbsp; <a href="ComplianceOverview.php">Compliance Overview</a> &nbsp;&raquo;&nbsp; <a href="PCI.php">PCI</a> &nbsp;&raquo;&nbsp;  DSS Windows 2008</span>
+            </span>
+         </div>
+      </div>
+<!-- end breadcrumbs -->
+
+      <br>
+      <div class="span-24 last">
+	     <div class="pagetitle2" style="margin-bottom:20px; margin-left:10px; float:left">DSS Windows 2008<br>
+		 </div>	  
+	  </div>
+
+
+
+<script>document.getElementById("complianceNav").className = "current-menu-item";</script>
+
+<div class="container">
+<div class="span-24 last">
+	<img src="SampleImages/PCI-DSSWin2008/Win2008_1-3.jpg">
+</div>
+<hr/>
+<div class="span-8">
+	<img src="SampleImages/PCI-DSSWin2008/Win2008_4.jpg">
+</div>
+<div class="span-3">
+	<a href="Win2008_checks.php" class="breadcrumbs">25 Checks</a>
+</div>
+<div class="span-8">
+	<img src="SampleImages/PCI-DSSWin2008/Win2008_5.jpg">
+</div>
+<div class="span-3 last">
+	<a href="Win2008_computers.php" class="breadcrumbs">11 Computers</a>
+</div>
+
+
+<br><br><br><br><br><br><br><br><br>
+
+<?php require 'includes/footer.php'; ?>
+</div>
+
