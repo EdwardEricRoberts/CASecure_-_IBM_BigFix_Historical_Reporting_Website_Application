1bbb3789e9ae4130a6c9f1059161230934373149
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:36:38 -0500

    Create footer.php

---
+++ CASecure_MSP_ver_1.0/includes/footer.php
@@ -0,0 +1,10 @@
+<br><br>
+</div>
+<div class="span-24 last prepend-top">
+         <hr />
+            <div style="float:right;">
+               &copy; 2020 CAS Severn, Inc.	
+            </div>
+      </div>
+</body>
+</html>
