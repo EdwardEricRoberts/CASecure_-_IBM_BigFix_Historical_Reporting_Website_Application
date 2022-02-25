8ab70291415870916b004ea30f44edec637362da
EdwardEricRoberts <77358927+EdwardEricRoberts@users.noreply.github.com>
2022-02-25 10:40:36 -0500

    Create MenuScrollTest.php

---
+++ CASecure_MSP_ver_1.0/MenuScrollTest.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	
+	<title>Trial of the scrolling dropdown</title>
+	
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection"/>
+	
+	<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js?ver=1.3.2'></script>
+	<script type="text/javascript" language="javascript" src="js/jquery.dropdown.js"></script>
+</head>
+
+<body>
+
+   
+      <ul class="dropdown">
+         <li><a href="#">Really Tall Menu</a>
+            <ul class="sub-menu">
+               <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+		       <li><a href="#">stuff</a></li>
+
+            </ul>
+         </li>
+      </ul>
+	  
+	  
+	  
+	  
+	  
+
+  
+</body>
+</html>
